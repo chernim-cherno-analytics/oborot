e644caaffbb70c492bd6d51997f7fab208ce4f63
chernim-cherno-analytics <kpustavov@gmail.com>
2026-05-14 20:26:03 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -e
-
-echo "=== Installing Python dependencies ==="
 pip install -r requirements.txt
+echo "Build complete"
 
-echo "=== Installing Node dependencies ==="
-npm install
-npm run build
-
-echo "=== Build complete ==="
 
 
